1d3f5d7c6912080261083d6ee8b1f432aec4ec39
frank <thmsfrank04@gmail.com>
2023-05-23 09:05:53 -0700

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+ # attr_accessor :,stead,detail,filter
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_list)
+        word_list.select { |w| w.chars.sort == @word.chars.sort && w.downcase != @word.downcase }
+        end
+end
+
+
+   
+      
+
+        
+      
